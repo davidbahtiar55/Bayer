95fe33022e3902cb9a4a5bf37cda1cf010ba18cc
David Bahtiar <davidbahtiar55@gmail.com>
2022-04-21 15:49:56 +0700

    Rewards Module

--- cypress/integration/BayerUAT/Rewards.js
+++ cypress/integration/BayerUAT/Rewards.js
@@ -8,12 +8,17 @@ describe('Location Demo', function(){
         cy.title().should('eq','Loyalty CMS');
     });
 
-    it('should redirect Dashboard CMS', function(){
+     it('should redirect Menu Rewards', function(){
         cy.get('.mb-3 > .form-control').type('cte@mail.com');
         cy.get('.mb-4 > .form-control').type('coba123');
         cy.get(':nth-child(1) > .btn').click();
-     })
-
-
+        cy.get(':nth-child(7) > .nav-link').click();
+        cy.get('.col.d-flex > a > .btn').click();
+        cy.get('#code').type('RWC001');
+        cy.get('#name').type('Buku');
+        cy.get('#point').type('15000');
+        cy.get('#stock').type('10');
+        cy.get('#type').select('Physical');
+     }) 
 
 })
