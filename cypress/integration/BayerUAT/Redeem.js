8ba3e2f4e347097c57d1d3864f5cc4b5628b1b2a
Dimas Mahendra <pdimasmahendra@gmail.com>
2022-05-10 18:10:20 +0700

    update automation redeem (ui)

--- cypress/integration/BayerUAT/Redeem.js
+++ cypress/integration/BayerUAT/Redeem.js
@@ -1,136 +1,99 @@
-//POST User
-//update bfore run test!
+beforeEach(() => {
+    cy.visit(Cypress.env("baseUrl"));
+    cy.get(user_form).type(Cypress.env("username"))
+    cy.get(pass_form).type(Cypress.env("password"))
+    cy.get(login_btn).click()
+})
 
-const userId = 898912
-const username = "dimasmahendrap4"
-const usernameNotFound = "dimasmahendrap4"
-const firstName = "Dimas"
-const lastName = "Mahendra"
-const email = "dimas2@gmail.com"
-const password = "Qwerty123@"
-const phone = "Qwerty123@"
-const userStatus = 0
+describe('Redemption', () => {
+  context('Add Redeemption', function () {
+    it('Add Redemption With Valid Data', function () {
+      cy.get(redeem_menu).click()
+      // cy.wait(3000)
+      cy.get(addRedeem_btn).click({ force: true })
+      cy.get(member_form).type(member, { force: true }).type(enter)
+      cy.get(description_form).type(desc)
+      cy.get(receiverName_form).type(receiverName)
+      cy.get(receiverPhone_form).type(receiverPhone)
+      cy.get(receiverAddress_form).type(receiverAddress)
+      cy.get(reward_form).type(reward_1, { force: true }).type(enter)
+      cy.get(reward_form).type(reward_2, { force: true }).type(enter)
+      cy.get(reward_form).type(reward_3, { force: true }).type(enter)
+      cy.get(qty_form_1).clear().type(qty)
+      cy.get(qty_form_2).clear().type(qty)
+      cy.get(qty_form_3).clear().type(qty)
 
-describe('POST User With Array', () => {    
-  it('With Array', () => {
-    cy.request({
-      method: 'POST',
-      url: `/user/createWithArray`,
-      body: [{
-        id: userId,
-        username: username,
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password,
-        phone: phone,
-        userStatus: userStatus
-      }],
-      failOnStatusCode: false
-    }).then(response => {
-        expect(response).property('status').to.equal(200);
-        expect(response.body).property('code').to.equal(200)
+      cy.get(totalPoint_obj).should('have.text', totalPoint) //Check Grand Total Redemption
+
+      cy.get(submit_btn).click()
+
+      //Check redemption table after submit form
+      cy.get(member_tb).should('have.text', member)
+      cy.get(receiverName_tb).should('have.text', receiverName)
+      cy.get(totalPoint_tb).should('have.text', '301.000')
+      cy.get(status_tb).should('have.text', waitingStatus)
     })
-  })
-})
 
-describe('DELETE & GET user', () => {
-  it('successfull delete order', () => {
-    cy.request({
-      method: 'DELETE',
-      url: `/user/${username}`,
-      failOnStatusCode: false
-    }).then(response => {
-      expect(response).property('status').to.equal(200);
-      // expect(response.body).property('id').to.not.be.oneOf([null, ""])
-      expect(response.body).property('message').to.equal(username)
-      
-    })
   })
 
-  it('Get user not found', () => {
-      cy.request({
-      method : 'GET',
-      url:`/user/${username}`, 
-      failOnStatusCode: false
-      }).then(response => {        
-          // expect(response.body).property('id').to.not.be.oneOf([null, ""])
-        expect(response).property('status').to.equal(404);
-        expect(response.body).property('message').to.equal('User not found')
-      })
-  })
-})
+  // context('Add Redeemption', function () {
+  //   it('Update Redemption With other Status', function () {
+  //     cy.get(redeem_menu).click()
+  //     cy.get(edit_btn).click({ force: true })
+  //     cy.get(status_form).select(completedStatus)
+  //     cy.get(submit_btn).click()
 
-describe('POST User With List', () => {    
-  it('With List', () => {
-    cy.request({
-      method: 'POST',
-      url: `/user/createWithList`,
-      body: [{
-        id: userId,
-        username: username,
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password,
-        phone: phone,
-        userStatus: userStatus
-      }],
-      failOnStatusCode: false
-    }).then(response => {
-        expect(response).property('status').to.equal(200);
-        expect(response.body).property('code').to.equal(200)
-    })
-  })
-})
+  //     //Check redemption table after submit form
+  //     cy.get(member_tb).should('have.text', member)
+  //     cy.get(receiverName_tb).should('have.text', receiverName)
+  //     cy.get(totalPoint_tb).should('have.text', '301.000')
+  //     cy.get(status_tb).should('have.text', completedStatus)
+  //   })
 
-describe('PUT Existing user', function () {
-   it('successfull put', () => {
-      cy.request({
-      method : 'PUT',
-      url:`/user/${username}`, 
-        body: {
-        id: userId,
-        username: username,
-        firstName: "Mahendra",
-        lastName: "Putra",
-        email: email,
-        password: password,
-        phone: phone,
-        userStatus: userStatus,
-      },
-      failOnStatusCode: false
-      }).then(response => {        
-        expect(response).property('status').to.equal(200);
-          // expect(response.body).property('id').to.not.be.oneOf([null, ""])
-        expect(response.body).property('code').to.equal(200)
-      
-      })
-  })
+  // })
 })
 
 
-
-describe('GET User', () => {  
-  it('successfull get user by usename', () => {
-      cy.request({
-      method : 'GET',
-      url:`/user/${username}`, 
-      failOnStatusCode: false
-      }).then(response => {        
-          // expect(response.body).property('id').to.not.be.oneOf([null, ""])
-        expect(response).property('status').to.equal(200);
-        expect(response.body).property('id').to.equal(userId)
-        expect(response.body).property('username').to.equal(username)
-        expect(response.body).property('firstName').to.equal('Mahendra')
-        expect(response.body).property('lastName').to.equal('Putra')
-        expect(response.body).property('email').to.equal(email)
-        expect(response.body).property('password').to.equal(password)
-        expect(response.body).property('phone').to.equal(phone)
-        expect(response.body).property('userStatus').to.equal(userStatus)
-      })
-  })
-  
-})
+//Variable
+const member = 'Dimas WA'
+const desc = 'Reward Reward Reward Reward Reward'
+const receiverName = 'Putra Herlambang'
+const receiverPhone = '085645213987'
+const receiverAddress = 'Jl. Awas Banyak Anak Kecil No. 45A-B'
+const reward_1 = 'GoPay'
+const reward_2 = 'Voucher McD'
+const reward_3 = 'Boneka Chucky'
+const qty = '2'
+const totalPoint = 'Total Point:  301.000'
+const waitingStatus = 'waiting'
+const processStatus = 'process'
+const shippingStatus = 'shipping'
+const completedStatus = 'completed'
+const rejectedStatus = 'rejected'
 
 
+
+//Object
+const user_form = '.mb-3 > .form-control'
+const pass_form = '.mb-4 > .form-control'
+const login_btn = ':nth-child(1) > .btn'
+const redeem_menu = ':nth-child(10) > .nav-link'
+const addRedeem_btn = '.col.d-flex > a > .btn'
+const member_form = '#react-select-2-input'
+const description_form = ':nth-child(2) > .form-control'
+const receiverName_form = '#receiverName'
+const receiverPhone_form = '#receiverPhone'
+const receiverAddress_form = ':nth-child(6) > .form-control'
+const reward_form = '#react-select-3-input'
+const qty_form_1 = 'tbody > :nth-child(1) > :nth-child(2) > .form-control'
+const qty_form_2 = ':nth-child(2) > :nth-child(2) > .form-control'
+const qty_form_3 = ':nth-child(3) > :nth-child(2) > .form-control'
+const enter = '{enter}'
+const totalPoint_obj = '.col-12 > .d-flex'
+const submit_btn = '.btn'
+const member_tb = 'tbody > :nth-child(1) > :nth-child(3)'
+const receiverName_tb = 'tbody > :nth-child(1) > :nth-child(4)'
+const totalPoint_tb = 'tbody > :nth-child(1) > :nth-child(9)'
+const status_tb = 'tbody > :nth-child(1) > :nth-child(10)'
+const edit_btn = '[href="#/redemptions/moderate/149"] > .btn'
+const status_form = '.form-select'
--- cypress/integration/BayerUAT/Transaction.js
+++ cypress/integration/BayerUAT/Transaction.js
@@ -1,23 +1,30 @@
 //<reference types="Cypress"/>
 
+//PLEASE UPDATE THIS VARIBLE BEFORE RUN TEST 
+const invoice1 = 'INV-022'
+const invoice2 = 'INV-022'
+const uniquecode = 'GJU022'
+
+
 beforeEach(() => {
     cy.visit(Cypress.env("baseUrl"));
     cy.get(user_form).type(Cypress.env("username"))
     cy.get(pass_form).type(Cypress.env("password"))
     cy.get(login_btn).click()
+    cy.wait(6000)
 })
 
-describe('Add Transaction', () => {
+describe('Transaction', () => {
     context('Upload Receipt', function () {
         it('Upload with Valid Data', function () {
             cy.get(transc_menu).click()
-            cy.get(addtransc_btn).click({force: true})
+            cy.get(addtransc_btn).click({ force: true })
             cy.get(memberReceipt_form).type(member, { force: true }).type(enter)
-            cy.get(invoice_form).type(invoice1, {force: true})
-            cy.get(transaction_form).type(total_transc, {force: true})
-            cy.get(retailer_form).type(retailer, {force: true}).type(enter)
+            cy.get(invoice_form).type(invoice1, { force: true })
+            cy.get(transaction_form).type(total_transc, { force: true })
+            cy.get(retailer_form).type(retailer, { force: true }).type(enter)
             cy.get(addressretail_form).type(address_retail, { force: true })
-            cy.get(date_form).type(date, {force : true})
+            cy.get(date_form).type(date, { force: true })
             cy.get(img_form).selectFile('cypress/fixtures/img/nota2.jpg', { action: 'drag-drop' })
             cy.get(status_form).select(status)
 
@@ -26,7 +33,7 @@ describe('Add Transaction', () => {
             cy.get(qty_form).type(qty1, { force: true })
             cy.get(addpoint_btn).click()
             cy.get(qty_form).clear()
-            cy.get('tbody > tr > :nth-child(1)').should('have.text', 'Mobil Diecast')
+            cy.get('tbody > tr > :nth-child(1)').should('have.text', 'Mobil Diecast')   
             cy.get('tbody > tr > :nth-child(3)').should('have.text', '11000')
 
             // ADD Point (Baju Baja)
@@ -35,25 +42,25 @@ describe('Add Transaction', () => {
             cy.get(addpoint_btn).click()
             cy.get('tbody > :nth-child(2) > :nth-child(1)').should('have.text', 'Baju Baja')
             cy.get('tbody > :nth-child(2) > :nth-child(3)').should('have.text', '10500')
+            cy.wait(5000)
 
-            cy.get(sumbit_btn).click()                    
+            cy.get(sumbit_btn).click()
             cy.get('tbody > :nth-child(1) > :nth-child(1)').should('have.text', invoice1)
             cy.get('tbody > :nth-child(1) > :nth-child(6)').should('have.text', 'upload-receipt')
             cy.get('tbody > :nth-child(1) > :nth-child(7)').should('have.text', member)
-            
+
         })
-        
+
     })
 
-    context('Unique Code', function () { 
+    context('Unique Code', function () {
         it('Input with Valid Data', function () {
-  
+
             cy.get(uniquecode_menu).click()
             cy.get('.col.d-flex > a > .btn').click()
             cy.get('#code').type(uniquecode)
             cy.get('#statuss').select('Available')
-            cy.get('#react-select-2-input').click()
-            cy.contains('Mobil Diecast').click()
+            cy.get('#react-select-2-input').type(action1, { force: true }).type(enter)
             cy.get('.btn').click()
             cy.get('tbody > :nth-child(1) > :nth-child(2)').should('have.text', 'Available')
             cy.wait(6000)
@@ -66,28 +73,26 @@ describe('Add Transaction', () => {
             cy.get(retailerCode_form).type(retailer, { force: true }).type(enter)
             cy.get(addressretail_form).type(address_retail, { force: true })
             cy.get(date_form).type(date, { force: true })
-            cy.get(uniquecode_form).type(uniquecode, {force: true}).type(enter)
-            
+            cy.get(uniquecode_form).type(uniquecode, { force: true }).type(enter)
+
             cy.get(sumbit_btn).click()
             cy.get('tbody > :nth-child(1) > :nth-child(1)').should('have.text', invoice2)
             cy.get('tbody > :nth-child(1) > :nth-child(6)').should('have.text', 'uniquecode')
             cy.get('tbody > :nth-child(1) > :nth-child(7)').should('have.text', member)
-            cy.wait(7000)
+            cy.wait(6000)
 
             cy.get(uniquecode_menu).click()
             cy.get('tbody > :nth-child(1) > :nth-child(2)').should('have.text', 'Not Available')
 
-            
+
         })
-        
+
     })
-}) 
+})
 
 //Variable
 const member = 'Dimas WA'
 const type = 'Unique Code'
-const invoice1 = 'INV-011' //Update this variable before run test
-const invoice2 = 'INV-012'
 const total_transc = '15000000'
 const retailer = 'ALFAMART'
 const address_retail = 'Jl. Tebet Timur'
@@ -99,7 +104,6 @@ const action1 = 'Mobil Diecast'
 const action2 = 'Baju Baja'
 const qty1 = '2'
 const qty2 = '3'
-const uniquecode = 'GJK456' //Update this variable before run test
 
 
 //Object
@@ -112,7 +116,7 @@ const addtransc_btn = '.col.d-flex > a > .btn'
 const memberReceipt_form = '#react-select-2-input'
 const memberCode_form = '#react-select-3-input'
 const enter = '{enter}'
-                   
+
 const type_form = '#react-select-4-input'
 const type_uniquecode = '//*[@value="uniquecode"]/input'
 const invoice_form = '#InvoiceNumber'
@@ -123,7 +127,7 @@ const retailerCode_form = '#react-select-6-input'
 const addressretail_form = '#RetailAddress'
 const date_form = '#TransactionDate'
 const img_form = '#ReceiptImage'
-const status_form = ':nth-child(9) > .col-sm-10 > .form-control' 
+const status_form = ':nth-child(9) > .col-sm-10 > .form-control'
 const action_form = '#react-select-4-input'
 const qty_form = '#Qty'
 const addpoint_btn = '#root > div:nth-child(2) > div.wrapper.d-flex.flex-column.min-vh-100.bg-light > div.body.flex-grow-1.px-3 > div > div > div > div > div.card-body > form > div:nth-child(12) > div:nth-child(1) > div:nth-child(4) > div.col-sm-10 > button'
